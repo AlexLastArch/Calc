8b569ec72c21574ad4c7be0b4998006f090ac38b
AlexLastArch <Arcantus11@gmail.com>
2020-07-03 13:13:31 +0300

    first commit

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+const dispaly = document.querySelector(".display");
+
+document.querySelectorAll(".digitstop button")
+.forEach( button => button.addEventListener('clic', digitPressed));
+
+document.querySelectorAll(".digitsmidle button")
+.forEach( button => button.addEventListener('clic', digitPressed));
+
+function digitPressed(ev) {
+   dispaly.value += ev.target.innerText;
+}
+
+document.querySelectorAll(".opers button")
+.forEach( button => button.addEventListener('clic', operPressed));
+
+document.querySelectorAll(".opers1 button")
+.forEach( button => button.addEventListener('clic', operPressed));
+
+
+function operPressed(ev) {
+    dispaly.value += ev.target.innerText;
+ }
+
+ document.querySelector(".eq").addEventListener("click", calculate);
+  
+ function calculate() {
+     dispaly.value = eval(display.value)
+ }
